c49ff09508ca55f3e944e00602334481f507d7e5
Madelen Yoo <rolemadelen@tuta.io>
2024-03-20 16:17:39 -0400

    feat: ascii art for time added

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::{self, Write};
-use std::time::{Duration, SystemTime};
+use std::time::{Duration};
 use std::thread::sleep;
 
 fn read_string() -> String {
@@ -18,6 +18,21 @@ fn setup(focus_session: &mut i32, break_session: &mut i32) {
     *break_session = read_string().trim().parse().expect("failed to parse a break_session");
 }
 
+fn merge_and_print(a: &str, b: &str) {
+    let a = a.to_string();
+    let b = b.to_string();
+    
+    let a: Vec<&str> = a.split('\n').collect();
+    let b: Vec<&str> = b.split('\n').collect();
+    
+    println!();
+    for i in 0..5 {
+        print!("{}   {}", a[i], b[i]);
+        println!();
+    }
+    println!();
+}
+
 fn main() {
     let mut focus_session: i32 = 0;
     let mut break_session: i32 = 0;
@@ -34,22 +49,47 @@ fn main() {
         }
     }
 
-    let mut focus_min = focus_session;
-    let mut break_min = break_session;
+    let ascii_art: [&str; 10] = [
+        "000000\n00  00\n00  00\n00  00\n000000",
+        "1111  \n  11  \n  11  \n  11  \n111111",
+        "222222\n     2\n222222\n2     \n222222",
+        "333333\n    33\n333333\n    33\n333333",
+        "44  44\n44  44\n444444\n    44\n    44",
+        "555555\n55    \n555555\n    55\n555555",
+        "666666\n66    \n666666\n66  66\n666666",
+        "777777\n    77\n    77\n    77\n    77",
+        "888888\n88  88\n888888\n88  88\n888888",
+        "999999\n99  99\n999999\n    99\n999999",
+    ];
+
+    let mut focus_min = focus_session-1;
+    let mut break_min = break_session-1;
     
     loop {
-        while focus_min > 0 {
-            println!("focus: {focus_min}");
+        while focus_min >= 0 {
+            print!("\x1B[2J");
+            println!("focus for {focus_session} minute(s).");
+
+            let tens = (focus_min / 10) as usize;
+            let ones = (focus_min % 10) as usize;
+            merge_and_print(ascii_art[tens], ascii_art[ones]);
+            
             sleep(Duration::new(60, 0));
             focus_min -= 1;
         }
-        focus_min = focus_session;
+        focus_min = focus_session-1;
+        
+        while break_min >= 0 {
+            print!("\x1B[2J");
+            println!("take a break for {break_session} minute(s).");
 
-        while break_min > 0 {
-            println!("break: {break_min}");
+            let tens = (break_min / 10) as usize;
+            let ones = (break_min % 10) as usize;
+            merge_and_print(ascii_art[tens], ascii_art[ones]);
+
             sleep(Duration::new(60, 0));
             break_min -= 1;
         }
-        break_min = break_session;
+        break_min = break_session-1;
     }
 }
