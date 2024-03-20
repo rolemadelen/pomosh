539a7ef3a2d7e09aa27e5359945f7995052e3519
rolemadelen <rolemadelen17@pm.me>
2024-03-20 19:33:12 -0400

    feat: color added + pomodoro count added

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use chrono::{Local, TimeDelta};
+use colored::Colorize;
 use std::io::{self, Write};
 use std::thread::sleep;
 use std::time::Duration;
@@ -12,19 +13,41 @@ fn read_string() -> String {
 }
 
 fn setup(focus_session: &mut i64, break_session: &mut i64) {
-    print!("How long is the focus session? (minutes): ");
-    io::stdout().flush().unwrap();
-    *focus_session = read_string()
-        .trim()
-        .parse()
-        .expect("failed to parse a focus_session");
+    let focus_bound = [5, 90];
+    let break_bound = [2, 90];
+    loop {
+        print!(
+            "How long is the focus session? ({lower}-{upper} minutes): ",
+            lower = focus_bound[0],
+            upper = focus_bound[1]
+        );
+        io::stdout().flush().unwrap();
+        *focus_session = read_string()
+            .trim()
+            .parse()
+            .expect("failed to parse a focus_session");
 
-    print!("How long is the break? (minutes): ");
-    io::stdout().flush().unwrap();
-    *break_session = read_string()
-        .trim()
-        .parse()
-        .expect("failed to parse a break_session");
+        if *focus_session >= 5 && *focus_session <= 90 {
+            break;
+        }
+    }
+
+    loop {
+        print!(
+            "How long is the short break? ({lower}-{upper} minutes): ",
+            lower = break_bound[0],
+            upper = break_bound[1]
+        );
+        io::stdout().flush().unwrap();
+        *break_session = read_string()
+            .trim()
+            .parse()
+            .expect("failed to parse a break_session");
+
+        if *break_session >= 2 && *break_session <= 90 {
+            break;
+        }
+    }
 }
 
 fn merge_and_print(a: &str, b: &str) {
@@ -36,7 +59,7 @@ fn merge_and_print(a: &str, b: &str) {
 
     println!();
     for i in 0..5 {
-        print!("{}   {}", a[i], b[i]);
+        print!("{}  {}", a[i].blue(), b[i].blue());
         println!();
     }
     println!();
@@ -79,36 +102,74 @@ fn main() {
         }
     }
 
+    let mut session_cnt = 0;
+
     loop {
         let mut focus_min = focus_session - 1;
         let mut break_min = break_session - 1;
+        let mut long_break = false;
 
         let (start, end) = get_session_duration(focus_session);
+        let books_emoji = [
+            char::from_u32(0x1f4d5).unwrap(),
+            char::from_u32(0x1f4d7).unwrap(),
+            char::from_u32(0x1f4d8).unwrap(),
+            char::from_u32(0x1f4d9).unwrap(),
+        ];
 
+        let square_emoji = char::from_u32(0x25fb).unwrap();
         while focus_min >= 0 {
             print!("\x1B[2J");
-            println!("focus for {focus_session} minute(s) ー ({start} - {end})");
+            for i in 0..=session_cnt {
+                print!("{} ", books_emoji[i % 4]);
+                if (i + 1) % 4 == 0 {
+                    println!();
+                }
+            }
+            for i in 0..4 - (session_cnt % 4) - 1 {
+                print!("{} ", square_emoji);
+                if (i + 1) % 4 == 0 {
+                    println!();
+                }
+            }
+            println!();
+            println!();
+            println!("focus: {focus_session} minutes / ({start} - {end})");
 
             let tens = (focus_min / 10) as usize;
             let ones = (focus_min % 10) as usize;
             merge_and_print(ascii_art[tens], ascii_art[ones]);
 
-            sleep(Duration::new(60, 0));
+            sleep(Duration::new(1, 0));
             focus_min -= 1;
         }
 
         let (start, end) = get_session_duration(break_session);
 
+        if (session_cnt + 1) % 4 == 0 {
+            long_break = true;
+            break_min <<= 1;
+        }
+
         while break_min >= 0 {
             print!("\x1B[2J");
-            println!("take a break for {break_session} minute(s) ー ({start} - {end})");
+            if long_break {
+                println!(
+                    "long break: {} minutes / ({start} - {end})",
+                    break_session << 1
+                );
+            } else {
+                println!("short break: {break_session} minutes / ({start} - {end})");
+            }
 
             let tens = (break_min / 10) as usize;
             let ones = (break_min % 10) as usize;
             merge_and_print(ascii_art[tens], ascii_art[ones]);
 
-            sleep(Duration::new(60, 0));
+            sleep(Duration::new(1, 0));
             break_min -= 1;
         }
+
+        session_cnt += 1;
     }
 }
